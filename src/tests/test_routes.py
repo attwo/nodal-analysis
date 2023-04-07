4f1f6d004059f0994b4c36e6543f09f1bb6aa64e
attwo <anntakefour@gmail.com>
2023-04-07 09:32:30 +0300

    wip

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,4 +1,3 @@
-import json
 import numpy as np
 import pytest
 from fastapi.testclient import TestClient
@@ -6,7 +5,7 @@ from fastapi.testclient import TestClient
 
 @pytest.fixture
 def generate_data_for_tests():
-    return json.dumps({
+    return {
                 "vlp": {
                     "q_liq": [0, 30, 60, 90, 120, 150],
                     "p_wf": [200, 190, 180, 175, 185, 200]
@@ -15,16 +14,16 @@ def generate_data_for_tests():
                     "q_liq": [0, 30, 60, 90, 120, 150],
                     "p_wf": [200, 180, 160, 140, 120, 100]
                 }
-            })
+            }
 
 
 def test_calc_model_success(api_client: TestClient, generate_data_for_tests):
-    resp = api_client.post('/nodal/calc', generate_data_for_tests)
+    resp = api_client.post('/nodal/calc', json=generate_data_for_tests)
     assert resp.status_code == 200
-    content = json.loads(resp.content)
+    content = resp.json()
     assert type(content) is list
     assert len(content) == 1
     item = content[0]
-    assert type(item) is dict
-    assert item.get('p_wf') == 200 and item.get('q_liq') == 0
-    assert content == [{'p_wf': 200, 'q_liq': 0}]
+    assert item
+    assert item['p_wf'] is not None
+    assert item['q_liq'] is not None
