27e310792d6fc955a611a7a15dfb8908d806720d
attwo <anntakefour@gmail.com>
2023-04-11 10:21:34 +0300

    feat(views): Реализован эндпоинт для выполнения УА, добавлен тест

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,11 +1,27 @@
-import numpy as np
 import pytest
 
 
 @pytest.fixture
 def generate_data_for_tests():
-    pass
+    return {
+        "vlp": {
+            "q_liq": [0, 30, 60, 90, 120, 150],
+            "p_wf": [200, 190, 180, 175, 185, 200],
+        },
+        "ipr": {
+            "q_liq": [0, 30, 60, 90, 120, 150],
+            "p_wf": [200, 180, 160, 140, 120, 100],
+        },
+    }
 
 
 def test_calc_model_success(api_client, generate_data_for_tests):
-    pass
+    resp = api_client.post("/nodal/calc", json=generate_data_for_tests)
+    assert resp.status_code == 200
+    content = resp.json()
+    assert type(content) is list
+    assert len(content) == 1
+    item = content[0]
+    assert item
+    assert item["p_wf"] is not None
+    assert item["q_liq"] is not None
